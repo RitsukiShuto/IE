ac8c9b6cfa0325c639f8e33315fc2345f1b30f19
RitsukiShuto <rtk410s@gmail.com>
2020-05-25 15:59:05 +0900

    Added need code

--- signal_processing/signal_processing02/2020-2-6.c
+++ signal_processing/signal_processing02/2020-2-6.c
@@ -12,19 +12,19 @@ cl 2020-2-6.c readWavHead.c writeWavHead.c
 by mokam@cis
 */
 
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
 #define FRAMESIZE 256
 
 /*こういう関数を使うよ、という宣言*/
-int readWavHead(FILE* fp, int* dataLength, unsigned long* fs, unsigned short* chNum, int* sampSize);
-int writeWavHead(FILE* fp, int dataLength, unsigned long fs, unsigned short  chNum, int sampSize);
+int readWavHead(FILE *fp, int *dataLength, unsigned long *fs, unsigned short *chNum, int *sampSize);
+int writeWavHead(FILE *fp, int dataLength, unsigned long fs, unsigned short chNum, int sampSize);
 
 void main()
 {
 	//変数の宣言
-	char inFile[] = "VoiceMono.wav"; //読み込みファイル名
+	char inFile[] = "VoiceMono.wav";	  //読み込みファイル名
 	char outFile[] = "VoiceMono_out.wav"; //読み込みファイル名
 	FILE *ifp, *ofp;
 	short dataIn[FRAMESIZE];
@@ -39,7 +39,8 @@ void main()
 	ofp = fopen(outFile, "wb");
 
 	/*Wavのヘッダを読む*/
-	if (readWavHead(ifp, &len, &fs, &chNum, &sampSize) < 0) {
+	if (readWavHead(ifp, &len, &fs, &chNum, &sampSize) < 0)
+	{
 		puts("入力ファイルのヘッダがうまく読めません");
 		exit(-1);
 	}
@@ -50,7 +51,7 @@ void main()
 	printf("1サンプルのビット数は %d ビット\n", sampSize);
 
 	//データ長からループの回数を計算。
-	fNum = (int)floor(len/ FRAMESIZE);
+	fNum = (int)floor(len / FRAMESIZE);
 
 	//fNumから書き込みファイルのサイズを計算
 	outlen = fNum * FRAMESIZE;
@@ -61,18 +62,16 @@ void main()
 	*/
 	writeWavHead(ofp, outlen, fs, chNum, sampSize);
 
-	for (int i = 0; i < fNum; i++) {
-		/*
-
-		ここにfreadとfwriteからなる文を書かないと完成しない。
-		資料を見て追記すること
-
-		*/
-
+	for (int i = 0; i < fNum; i++)
+	{
+		fread(&dataIn[i], sizeof(short), 1, ifp);
+		for (int j = 0; j < FRAMESIZE; j++)
+		{
+			dataOut[j] = dataIn[j];
+		}
+		fwrite(dataOut, sizeof(short), 256, ofp);
 	}
-
 
 	fclose(ifp);
 	fclose(ofp);
-
 }
