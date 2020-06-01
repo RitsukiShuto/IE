620ae780727f558abafbfbce5454a24419bfbb38
RitsukiShuto <rtk410s@gmail.com>
2020-06-01 15:53:17 +0900

    Added need code(L72)

--- signal_processing/signal_processing03/2020-3-4.c
+++ signal_processing/signal_processing03/2020-3-4.c
@@ -10,21 +10,22 @@ cl 2020-3-4.c readWavHead.c writeWavHead.c
 by mokam@cis
 */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 #define FRAMESIZE 256
 
 /*こういう関数を使うよ、という宣言*/
-int readWavHead(FILE* fp, int* dataLength, unsigned long* fs, unsigned short* chNum, int* sampSize);
-int writeWavHead(FILE* fp, int dataLength, unsigned long fs, unsigned short  chNum, int sampSize);
-int volChange(short* in, short* out, float val, int len);
+int readWavHead(FILE *fp, int *dataLength, unsigned long *fs, unsigned short *chNum, int *sampSize);
+int writeWavHead(FILE *fp, int dataLength, unsigned long fs, unsigned short chNum, int sampSize);
+int volChange(short *in, short *out, float val, int len);
 
-void main(int argc, char* argv[]) {
-	char inFile[256]; //読み込みファイル名を格納する変数
+void main(int argc, char *argv[])
+{
+	char inFile[256];  //読み込みファイル名を格納する変数
 	char outFile[256]; //出力ファイル名を格納する変数
-	FILE* ifp, * ofp;
+	FILE *ifp, *ofp;
 	short dataIn[FRAMESIZE];
 	short dataOut[FRAMESIZE];
 	int len, sampSize, outlen;
@@ -33,7 +34,8 @@ void main(int argc, char* argv[]) {
 	int fNum;
 
 	/*コマンドラインの引数が足りないと動かないので、数をチェックする*/
-	if (argc < 3) {
+	if (argc < 3)
+	{
 		printf("引数に二つ以上の文字列を書いてください\n");
 		exit(0);
 	}
@@ -48,7 +50,8 @@ void main(int argc, char* argv[]) {
 	ofp = fopen(outFile, "wb");
 
 	/*Wavのヘッダを読む*/
-	if (readWavHead(ifp, &len, &fs, &chNum, &sampSize) < 0) {
+	if (readWavHead(ifp, &len, &fs, &chNum, &sampSize) < 0)
+	{
 		puts("入力ファイルのヘッダがうまく読めません");
 		exit(-1);
 	}
@@ -63,25 +66,26 @@ void main(int argc, char* argv[]) {
 
 	writeWavHead(ofp, outlen, fs, chNum, sampSize);
 
-	for (int i = 0; i < fNum; i++) {
+	for (int i = 0; i < fNum; i++)
+	{
 		fread(dataIn, sizeof(short), FRAMESIZE, ifp);
 
-		/*読み込んだデータを関数を読んで加工する文を追記する。資料をよく読み書くこと*/
+		volChange(dataIn, dataOut, 0.5, FRAMESIZE);
 
 		fwrite(dataOut, sizeof(short), FRAMESIZE, ofp);
 	}
 
 	fclose(ifp);
 	fclose(ofp);
-
-
 }
 
-int volChange(short* in, short* out, float val, int len) {
+int volChange(short *in, short *out, float val, int len)
+{
 	float ff;
 	int i;
 
-	for (i = 0; i < len; i++) {
+	for (i = 0; i < len; i++)
+	{
 		ff = *in * val;
 		*out = (short)floor(ff);
 		in++;
@@ -89,5 +93,4 @@ int volChange(short* in, short* out, float val, int len) {
 	}
 
 	return i;
-
 }
