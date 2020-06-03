cf61e8428a0bbb4406caf44a99d61d67d4a9637a
RitsukiShuto <rtk410s@gmail.com>
2020-06-03 21:52:11 +0900

    Download code

--- signal_processing/signal_processing02/2020-2-6.c
+++ signal_processing/signal_processing02/2020-2-6.c
@@ -12,20 +12,20 @@ cl 2020-2-6.c readWavHead.c writeWavHead.c
 by mokam@cis
 */
 
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
 
 #define FRAMESIZE 256
 
 /*こういう関数を使うよ、という宣言*/
-int readWavHead(FILE *fp, int *dataLength, unsigned long *fs, unsigned short *chNum, int *sampSize);
-int writeWavHead(FILE *fp, int dataLength, unsigned long fs, unsigned short chNum, int sampSize);
+int readWavHead(FILE* fp, int* dataLength, unsigned long* fs, unsigned short* chNum, int* sampSize);
+int writeWavHead(FILE* fp, int dataLength, unsigned long fs, unsigned short  chNum, int sampSize);
 
-void main(int argc, char *argv[])
+void main()
 {
 	//変数の宣言
-	char inFile[256];  //読み込みファイル名
-	char outFile[256]; //読み込みファイル名
+	char inFile[] = "VoiceMono.wav"; //読み込みファイル名
+	char outFile[] = "VoiceMono_out.wav"; //読み込みファイル名
 	FILE *ifp, *ofp;
 	short dataIn[FRAMESIZE];
 	short dataOut[FRAMESIZE];
@@ -34,23 +34,12 @@ void main(int argc, char *argv[])
 	unsigned short chNum;
 	int fNum;
 
-	// コマンドライン引数をチェック
-	if (argc < 3)
-	{
-		puts("コマンドライン引数が足りません。");
-		exit(0);
-	}
-
-	strcpy(inFile, argv[1]);
-	strcpy(outFile, argv[2]);
-
 	//ファイルを開く
 	ifp = fopen(inFile, "rb");
 	ofp = fopen(outFile, "wb");
 
 	/*Wavのヘッダを読む*/
-	if (readWavHead(ifp, &len, &fs, &chNum, &sampSize) < 0)
-	{
+	if (readWavHead(ifp, &len, &fs, &chNum, &sampSize) < 0) {
 		puts("入力ファイルのヘッダがうまく読めません");
 		exit(-1);
 	}
@@ -61,7 +50,7 @@ void main(int argc, char *argv[])
 	printf("1サンプルのビット数は %d ビット\n", sampSize);
 
 	//データ長からループの回数を計算。
-	fNum = (int)floor(len / FRAMESIZE);
+	fNum = (int)floor(len/ FRAMESIZE);
 
 	//fNumから書き込みファイルのサイズを計算
 	outlen = fNum * FRAMESIZE;
@@ -72,16 +61,18 @@ void main(int argc, char *argv[])
 	*/
 	writeWavHead(ofp, outlen, fs, chNum, sampSize);
 
-	for (int i = 0; i < fNum; i++)
-	{
-		fread(dataIn, sizeof(short), FRAMESIZE, ifp);
-		for (int j = 0; j < FRAMESIZE; j++)
-		{
-			dataOut[j] = dataIn[j];
-		}
-		fwrite(dataOut, sizeof(short), FRAMESIZE, ofp);
+	for (int i = 0; i < fNum; i++) {
+		/*
+
+		ここにfreadとfwriteからなる文を書かないと完成しない。
+		資料を見て追記すること
+
+		*/
+
 	}
+
 
 	fclose(ifp);
 	fclose(ofp);
+
 }
