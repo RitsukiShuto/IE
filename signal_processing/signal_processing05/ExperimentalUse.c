58482eaa9a998678571a108967d1e7846146d5d5
ritsukishuto <rtk410s@gmail.com>
2020-06-21 21:34:44 +0900

    ファイル名作成コードを追加

--- signal_processing/signal_processing05/ExperimentalUse.c
+++ signal_processing/signal_processing05/ExperimentalUse.c
@@ -15,15 +15,18 @@ Coding 'Shift JIS'
 */
 #include<stdio.h>
 #include<stdio.h>
+#include<stdlib.h>
 
 // prototype宣言
 int readWavHead(FILE* fp, int* dataLength, unsigned long* fs, unsigned short* chNum, int* sampSize);
 int writeWavHead(FILE* fp, int dataLength, unsigned long fs, unsigned short  chNum, int sampSize);
 int CT_fft(double* x, double* y, int n, int fr);
 
-void main(){
+void main(int argc, char *argv[]){
+    char ioFile[256];   // 出力ファイルの素
     char inFile[256];   // 入力ファイル
     char outFile[256];  // 出力ファイル
+    char csvFile[256];  // CSVファイル
     FILE *ifp, *ofp;
 
     int FRAMESIZE;
@@ -39,6 +42,49 @@ void main(){
     int N1, N2;
 
     char yN = 'n';
+
+    if(argc < 2){
+        puts("コマンドライン引数が足りません");
+        exit(0);
+    }
+
+	strcpy(inFile, argv[1]);    // 入力ファイルをコピー
+    strcpy(ioFile, argv[1]);
+
+    for(int i = 0;i < 256;i++){
+        if(ioFile[i] == '.'){  // '.'を検索
+            for(int j = i;j < length(inFile);j++){
+                // 拡張子を'\0'で埋める
+                ioFile[j] = '\0';
+                if(ioFile[j + 1] == '\0'){  // Null文字を検索
+                    break;
+                }
+            }
+        }
+    }
+
+    // 出力ファイル名を作成
+    strcpy(csvFile, strcat("PowSpectorum", ioFile, ".csv"));
+    strcpy(outFile, strcat("Out", ioFile, ".wav"));
+
+	printf("input file is %s.\n", inFile);
+	printf("output file is %s.\n", outFile);
+    printf("csv file is %s.\n", csvFile);
+
+	ifp = fopen(inFile, "rb");
+	ofp = fopen(outFile, "wb");
+
+    // wavヘッダを読み込み
+	if (readWavHead(ifp, &len, &fs, &chNum, &sampSize) < 0) {
+		puts("入力ファイルのヘッダがうまく読めません");
+		exit(-1);
+	}
+
+	// wavパラメータを出力
+	printf("ファイルの長さは %d サンプル\n", len);
+	printf("サンプリング周波数は %d Hz\n", fs);
+	printf("チャネル数は %d\n", chNum);
+	printf("1サンプルのビット数は %d ビット\n", sampSize);
 
     while(yN == 'N' || 'n'){
         puts("各種パラメータを入力");
