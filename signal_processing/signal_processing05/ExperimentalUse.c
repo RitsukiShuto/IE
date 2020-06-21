1974887dc3bfccc5c755f64355cbb5b481e7a7a7
ritsukishuto <rtk410s@gmail.com>
2020-06-21 21:11:58 +0900

    パラメータ定義用コードを追加

--- signal_processing/signal_processing05/ExperimentalUse.c
+++ signal_processing/signal_processing05/ExperimentalUse.c
@@ -13,3 +13,47 @@ Coding 'Shift JIS'
     ExUse.exe <入力wavファイル>
 
 */
+#include<stdio.h>
+#include<stdio.h>
+
+// prototype宣言
+int readWavHead(FILE* fp, int* dataLength, unsigned long* fs, unsigned short* chNum, int* sampSize);
+int writeWavHead(FILE* fp, int dataLength, unsigned long fs, unsigned short  chNum, int sampSize);
+int CT_fft(double* x, double* y, int n, int fr);
+
+void main(){
+    char inFile[256];   // 入力ファイル
+    char outFile[256];  // 出力ファイル
+    FILE *ifp, *ofp;
+
+    int FRAMESIZE;
+
+    int len, sampSize, outlen;
+    unsigned long fs;
+    unsigned short chNum;
+    int fNum;
+
+    double GAIN;
+    int cut_Hz_N1, cut_Hz_N2;
+    int N1, N2;
+
+    char yN = 'n';
+
+    while(yN == 'N' || 'n'){
+        puts("各種パラメータを入力");
+        printf("FRAMESIZE = ");     scanf("%d", &FRAMESIZE);
+        printf("GAIN = ");          scanf("%lf", &GAIN);
+        printf("除去したい周波数帯域の最小値を入力[Hz]: ");
+        scanf_s("%lf", &cut_Hz_N1);
+        printf("除去したい周波数帯域の最大値を入力[Hz]: ");
+	    scanf_s("%lf", &cut_Hz_N2);
+
+        printf("FRAMESIZE: %d\n", FRAMESIZE);
+        printf("GAIN: %d\n", GAIN);
+    	printf("%lf[Hz]から%lf[Hz]を除去\n", cut_Hz_N1, cut_Hz_N2);
+	    printf("操作対象の要素番号は[%d]から[%d]\n", N1, N2);
+
+        puts("以上のパラメータで実行してよろしいですか(y/N):");
+        scanf("%c", &yN);
+    }
+}
