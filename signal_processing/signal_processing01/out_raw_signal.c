f90e3858b30c38161a144ee849414669c290a4cf
ritsukishuto <rtk410s@gmail.com>
2020-07-02 21:14:58 +0900

    コードを修正(L16)

--- signal_processing/signal_processing01/out_raw_signal.c
+++ signal_processing/signal_processing01/out_raw_signal.c
@@ -13,14 +13,14 @@ void main()
 {
 	char inFile[] = "1000Sine-8k.raw"; // 読み込みファイル名
 	FILE *ifp;						   // ファイルに紐づけるポインタ. どこまで読んだかも記録されている
-	short dataln;					   // 読み込んむデータの領域(1要素)
+	short dataIn;					   // 読み込んむデータの領域(1要素)
 
 	ifp = fopen(inFile, "rb"); //ファイルを読み込みでモードで開く。これ以降はifpでファイルを操作する
 
 	for (int i = 0; i < 8000; i++)
 	{
-		fread(&dataln, sizeof(short), 1, ifp); // ファイルを読み出し => 'dataln'に格納される
-		printf("%d\n", dataln);				   // データを出力
+		fread(&dataIn, sizeof(short), 1, ifp); // ファイルを読み出し => 'dataln'に格納される
+		printf("%d\n", dataIn);				   // データを出力
 	}
 
 	fclose(ifp); //ファイルを閉じる
